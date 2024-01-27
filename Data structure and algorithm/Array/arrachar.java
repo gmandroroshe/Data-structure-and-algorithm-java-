6d986f13ead6931529165382a6bf5e031f23cc6b
G.M. Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-01-27 10:04:02 +0530

    Add files via upload

---
+++ Data structure and algorithm/Array/arrachar.java
@@ -0,0 +1,10 @@
+//program date 2024.01.27
+
+public class arrachar {
+    public static void main(String[] args) {
+        char[] arra = {'A','B','C'};
+
+        System.out.println(arra[1]);
+        //chack length code = System.out.println(arra.length);
+    }
+}
---
+++ Data structure and algorithm/Array/arrayln.java
@@ -0,0 +1,13 @@
+//program date 2024.01.27
+
+public class arrayln {
+
+    public static void main(String[] args) {
+        int[] arra = {10,20,30,40,50};
+
+        System.out.println(arra[2]);
+    
+        //chack length code = System.out.println(arra.length);
+    }
+}
+
